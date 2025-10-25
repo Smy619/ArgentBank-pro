80cb99ff70702c7531a17239560c8ac0ed6f4b4e
smy619 <solene.s2025@gmail.com>
2025-10-25 18:55:06 +0200

    Fix DB to use MONGODB_URI for cloud deployment

--- Backend/server/database/connection.js
+++ Backend/server/database/connection.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose')
 const databaseUrl =
-  process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
+  process.env.MONGODB_URI || 'mongodb://localhost/argentBankDB'
 
 module.exports = async () => {
   try {
-    await mongoose.connect(databaseUrl, { useNewUrlParser: true })
+    await mongoose.connect(databaseUrl, { 
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     console.log('Database successfully connected')
   } catch (error) {
     console.error(`Database Connectivity Error: ${error}`)
--- Backend/server/server.js
+++ Backend/server/server.js
@@ -1,44 +1,38 @@
-require('dotenv').config();
+require('dotenv').config()
 
 const express = require('express')
-const dotEnv = require('dotenv')
 const cors = require('cors')
 const swaggerUi = require('swagger-ui-express')
 const yaml = require('yamljs')
 const swaggerDocs = yaml.load('./swagger.yaml')
 const dbConnection = require('./database/connection')
 
-dotEnv.config()
-
 const app = express()
 const PORT = process.env.PORT || 3001
 
-// Connect to the database
+// DB connect
 dbConnection()
 
-// Handle CORS issues
+// Middleware
 app.use(cors())
-
-// Request payload middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// Handle custom routes
+// Routes
 app.use('/api/v1/user', require('./routes/userRoutes'))
-
 app.use('/api/v1/accounts', require('./routes/accountRoutes'))
-
 app.use('/api/v1/transactions', require('./routes/transactionRoutes'))
 
-// API Documentation
+// Swagger DEV only
 if (process.env.NODE_ENV !== 'production') {
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 }
 
-app.get('/', (req, res, next) => {
-  res.send('Hello from my Express server v2!')
+app.get('/', (req, res) => {
+  res.send(' Hello from ArgentBank Backend!')
 })
 
 app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`)
+  console.log(`Backend running on port ${PORT}`)
 })
+
